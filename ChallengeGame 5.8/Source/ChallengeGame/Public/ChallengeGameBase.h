222c8727fae897a3bad94fbd61700661c4075aa3
Luvchan <vergilwu@yeah.net>
2026-09-17 23:50:50 +0800

    接入codex，转盘类型

---
+++ ChallengeGame 5.8/Source/ChallengeGame/Public/ChallengeGameBase.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class CHALLENGEGAME_API ChallengeGameBase
+{
+public:
+	ChallengeGameBase();
+	~ChallengeGameBase();
+};
